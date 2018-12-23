e82e7a3b3950528f0c095e4473d27a4e2856f1ce
kurokochin <rickyputra98@gmail.com>
2018-12-23 23:07:38 +0700

    add token checker

--- tokens/views.py
+++ tokens/views.py
@@ -2,6 +2,7 @@ import random, string
 from django.shortcuts import render
 from .serializers import TokenSerializer
 from .models import Token
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -13,3 +14,11 @@ class TokenView(APIView):
 		token = Token(payload=magic_string)
 		token.save()
 		return Response(TokenSerializer(token).data)
+
+	def post(self, request, format=None):
+		payload = request.data['payload']
+		try:
+			token = Token.objects.get(payload=payload)
+			return Response(status=status.HTTP_200_OK)
+		except Token.DoesNotExist:
+			return Response(status=status.HTTP_400_BAD_REQUEST)
